752b293590c5d84feadcc60cf402606f0dfdcdce
angelaldrete <angel.aldrete96@gmail.com>
2023-10-09 14:34:40 -0700

    Added react icons

--- app/modules/Skills.tsx
+++ app/modules/Skills.tsx
@@ -1,7 +1,142 @@
 import React from "react";
+import { AiFillHtml5 } from "react-icons/ai";
+import {
+  BiLogoCss3,
+  BiLogoJavascript,
+  BiLogoJquery,
+  BiLogoPhp,
+  BiLogoReact,
+  BiLogoAngular,
+  BiLogoVuejs,
+  BiLogoWordpress,
+  BiLogoMagento,
+  BiLogoDocker,
+  BiLogoKubernetes,
+  BiLogoAws,
+  BiLogoPython,
+  BiLogoNodejs,
+  BiLogoFlutter,
+  BiLogoJava,
+  BiLogoGoLang,
+} from "react-icons/bi";
+import {
+  DiCodeigniter,
+  DiJenkins,
+  DiSwift,
+  DiIonic,
+  DiMysql,
+} from "react-icons/di";
+import { FaLaravel } from "react-icons/fa";
+import {
+  SiAzuredevops,
+  SiDotnet,
+  SiPrometheus,
+  SiGrafana,
+} from "react-icons/si";
 
 const Skills = () => {
-  return <div></div>;
+  const skills = [
+    {
+      icon: <AiFillHtml5 />,
+    },
+    {
+      icon: <BiLogoCss3 />,
+    },
+    {
+      icon: <BiLogoJavascript />,
+    },
+    {
+      icon: <BiLogoJquery />,
+    },
+    {
+      icon: <BiLogoPhp />,
+    },
+    {
+      icon: <DiCodeigniter />,
+    },
+    {
+      icon: <FaLaravel />,
+    },
+    {
+      icon: <BiLogoReact />,
+    },
+    {
+      icon: <BiLogoAngular />,
+    },
+    {
+      icon: <BiLogoVuejs />,
+    },
+    {
+      icon: <BiLogoWordpress />,
+    },
+    {
+      icon: <BiLogoMagento />,
+    },
+    {
+      icon: <BiLogoDocker />,
+    },
+    {
+      icon: <BiLogoKubernetes />,
+    },
+    {
+      icon: <BiLogoAws />,
+    },
+    {
+      icon: <BiLogoPython />,
+    },
+    {
+      icon: <BiLogoNodejs />,
+    },
+    {
+      icon: <BiLogoFlutter />,
+    },
+    {
+      icon: <BiLogoJava />,
+    },
+    {
+      icon: <BiLogoGoLang />,
+    },
+    {
+      icon: <DiJenkins />,
+    },
+    {
+      icon: <DiSwift />,
+    },
+    {
+      icon: <DiIonic />,
+    },
+    {
+      icon: <DiMysql />,
+    },
+    {
+      icon: <SiAzuredevops />,
+    },
+    {
+      icon: <SiDotnet />,
+    },
+    {
+      icon: <SiPrometheus />,
+    },
+    {
+      icon: <SiGrafana />,
+    },
+  ];
+
+  return (
+    <section className="skills">
+      <h2 className="skills__title">Skills</h2>
+      <p className="skills_description">
+        Overall skills I've gathered by professional and personal experience
+      </p>
+      <ul className="skills__list">
+        {skills.map((skill, index) => (
+          <li key={index} className="skills__list-item">
+            {skill.icon}
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
 };
 
 export default Skills;
