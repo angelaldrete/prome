e639c57866d7273a323d7b2c9253bada32f99c5d
Angel Aldrete <42364950+angelaldrete@users.noreply.github.com>
2023-10-14 01:13:15 +0000

    Added project links

--- app/modules/Projects.tsx
+++ app/modules/Projects.tsx
@@ -1,36 +1,41 @@
 "use client";
 import React from "react";
 import Spline from "@splinetool/react-spline";
+import Link from "next/link";
 
 const Projects = () => {
   const projects = [
     {
       title: "Develomancy",
       description: "A blog about software development and other things.",
+      link: "https://develomancy.vercel.app",
       model: (
-        <Spline scene="https://prod.spline.design/5ZTaGDXthyzCSA08/scene.splinecode" />
+        <Spline scene="https://prod.spline.design/vMhsTCRtH6OuR56x/scene.splinecode" />
       ),
     },
     {
       title: "The Knob Studio",
       description:
         "An interactive website where I share my music as a producer.",
-      graphic: (
-        <Spline scene="https://prod.spline.design/5ZTaGDXthyzCSA08/scene.splinecode" />
+      link: "https://theknobstudio.vercel.app",
+      model: (
+        <Spline scene="https://prod.spline.design/z87rEp9IY5gDVOYl/scene.splinecode" />
       ),
     },
     {
       title: "Diagnosys",
       description: "A system for medical diagnosis and metrics.",
-      graphic: (
-        <Spline scene="https://prod.spline.design/5ZTaGDXthyzCSA08/scene.splinecode" />
+      link: "https://diagnosys.vercel.app",
+      model: (
+        <Spline scene="https://prod.spline.design/I17j-YJ4Natf3pBv/scene.splinecode" />
       ),
     },
     {
       title: "One Pillow",
       description: "A reservation system for different purposes.",
-      graphic: (
-        <Spline scene="https://prod.spline.design/5ZTaGDXthyzCSA08/scene.splinecode" />
+      link: "https://onepillow.vercel.app",
+      model: (
+        <Spline scene="https://prod.spline.design/YM2QCSVWcYt-BYgk/scene.splinecode" />
       ),
     },
   ];
@@ -45,12 +50,19 @@ const Projects = () => {
       <ul className="projects__list">
         {projects.map((project, index) => (
           <li className="projects__item" key={index}>
-            <div className="projects__item--graphic">{project.graphic}</div>
+            <div className="projects__item--graphic">{project.model}</div>
             <div className="projects__item--content">
               <h3 className="projects__item--title">{project.title}</h3>
               <p className="projects__item--description">
                 {project.description}
               </p>
+              <Link
+                className="projects__item--link"
+                href={project.link}
+                target="_blank"
+              >
+                Visit
+              </Link>
             </div>
           </li>
         ))}
